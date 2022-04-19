634e976b47699b18bbbfec8a631e2d81d357b1c3
Jhan Ignacio <jhanignacio26@gmail.com>
2022-04-18 21:38:01 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoHotel/Logica/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoHotel.Logica
+{
+    public class Conexion
+    {
+        //public static string CN = "Data Source=srvmtd2.database.windows.net;Initial Catalog=Mantenimientos;User Id=administrador;Password=JC2406962020jc2021";
+        public static string CN = "Data Source=JHANPC\\SQLEXPRESS;Database=DB_HOTEL;User Id=sa;Password=12345678;Trusted_Connection=False;MultipleActiveResultSets=True";
+        //public static string CN = "Data Source=JHANPC\\SQLEXPRESS;Initial Catalog=DB_HOTEL;Integrated Security=True";
+    }
+}
